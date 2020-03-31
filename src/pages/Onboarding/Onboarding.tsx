651f43bf48220b992efeaac9e2890b0a829dcc43
Terence Pae <terencepae@gmail.com>
2020-03-31 00:12:26 -0700

    added ci and onboarding with google

--- src/pages/Onboarding/Onboarding.tsx
+++ src/pages/Onboarding/Onboarding.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Pane } from 'evergreen-ui';
 import { Button } from 'react-bootstrap';
-import { signInWithFacebook } from '../../services/firebase';
+import { signInWithFacebook, signInWithGoogle } from '../../services/firebase';
 
 const Onboarding: React.FC<{}> = () => {
-  const handleSignIn = React.useCallback(() => {
+  const handleSignInFacebook = React.useCallback(() => {
     signInWithFacebook();
   }, []);
 
+  const handleSignInGoogle = React.useCallback(() => {
+    signInWithGoogle();
+  }, []);
+
   return (
-    <Pane width="100%" height="100vh" display="flex" alignItems="center" justifyContent="center">
-      <Button size="lg" onClick={handleSignIn}>Sign in with Facebook</Button>
+    <Pane width="100%" height="100vh" display="flex" alignItems="center" justifyContent="center" flexDirection="column">
+      <Button size="lg" onClick={handleSignInFacebook}>Sign in with Facebook</Button>
+      <Pane height="15px" />
+      <Button size="lg" onClick={handleSignInGoogle}>Sign in with Google</Button>
     </Pane>
   );
 };
