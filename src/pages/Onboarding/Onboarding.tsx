0723b55c576b0ede69e68164275582f43d110b19
Terence Pae <terencepae@gmail.com>
2020-03-31 17:17:48 -0700

    added onboarding screen

--- src/pages/Onboarding/Onboarding.tsx
+++ src/pages/Onboarding/Onboarding.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Pane } from 'evergreen-ui';
-import { Button } from 'react-bootstrap';
+import {
+  Button, Container, Col, Row,
+} from 'react-bootstrap';
+import styles from './Onboarding.module.scss';
 import { signInWithFacebook, signInWithGoogle } from '../../services/firebase';
 
 const Onboarding: React.FC<{}> = () => {
@@ -13,10 +16,42 @@ const Onboarding: React.FC<{}> = () => {
   }, []);
 
   return (
-    <Pane width="100%" height="100vh" display="flex" alignItems="center" justifyContent="center" flexDirection="column">
-      <Button size="lg" onClick={handleSignInFacebook}>Sign in with Facebook</Button>
-      <Pane height="15px" />
-      <Button size="lg" onClick={handleSignInGoogle}>Sign in with Google</Button>
+    <Pane className={styles.container}>
+      <img className={styles.logo} src="/header-logo.png" alt="Froyo Party" />
+      <Container>
+        <Row>
+          <Col style={{ justifyContent: 'center' }} className={styles.col} xs={12} md={6}>
+            <img className={styles.froyos} src="/froyos.png" alt="Froyos" />
+          </Col>
+          <Col className={styles.col} xs={12} md={6}>
+            <Pane display="flex" flexDirection="column">
+              <Pane>
+                <h1 className={styles.textHeader}>The Anti-Lonely App</h1>
+                <p className={styles.text}>
+                  Social distancing has never been so sweet. Virtually connect over topics
+                  and interests with like minded people. Discuss, learn and collaborate in a
+                  safe environment. We’re in this together.
+                </p>
+              </Pane>
+              <Pane className={styles.buttonContainer}>
+                <Button
+                  className={styles.facebookButton}
+                  onClick={handleSignInFacebook}
+                >
+                  Sign in with Facebook
+                </Button>
+                <Button
+                  className={styles.googleButton}
+                  onClick={handleSignInGoogle}
+                >
+                  Sign in with Google
+                </Button>
+              </Pane>
+            </Pane>
+          </Col>
+        </Row>
+      </Container>
+      <img className={styles.footer} src="/wave.svg" alt="Wave" />
     </Pane>
   );
 };
