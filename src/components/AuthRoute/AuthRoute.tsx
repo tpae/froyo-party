ba0d7f8c1551f2b2ac3df69880794069769e6716
Terence Pae <terencepae@gmail.com>
2020-04-08 23:52:40 -0700

    fixed issue with unauthenticated redirect

--- src/components/AuthRoute/AuthRoute.tsx
+++ src/components/AuthRoute/AuthRoute.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, useLocation } from 'react-router-dom';
 
 const AuthRoute = ({ component, isAuthenticated, ...rest }: any) => {
+  const location = useLocation();
+  const { from } = location.state || { from: { pathname: '/lobby' } };
   const routeComponent = (props: any) => (
     !isAuthenticated
       ? React.createElement(component, props)
-      : <Redirect to={{ pathname: '/lobby' }} />
+      : <Redirect to={from} />
   );
   return <Route {...rest} render={routeComponent} />;
 };
--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({ component, isAuthenticated, ...rest }: any) => {
+  const location = useLocation();
   const RouteComponent = (props: any) => (
     isAuthenticated
       ? React.createElement(component, props)
-      : <Redirect to={{ pathname: '/login' }} />
+      : <Redirect to={{ pathname: '/login', state: { from: location } }} />
   );
   return <Route {...rest} component={RouteComponent} />;
 };
